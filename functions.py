b7e9161fb5ec1a884829caf7c3414880778b0fda
guinafelix <oguilhermeleitefelix@gmail.com>
2022-09-26 22:24:09 -0300

    is_dnn function

--- functions.py
+++ functions.py
@@ -1,7 +1,6 @@
 """The goal in this module is to define functions that take a formula as input and
 do some computation on its syntactic structure. """
 
-
 from formula import *
 
 
@@ -27,7 +26,6 @@ def subformulas(formula):
     This piece of code prints p, s, (p v s), (p → (p v s))
     (Note that there is no repetition of p)
     """
-
     if isinstance(formula, Atom):
         return {formula}
     if isinstance(formula, Not):
@@ -37,10 +35,12 @@ def subformulas(formula):
         sub2 = subformulas(formula.right)
         return {formula}.union(sub1).union(sub2)
 
+
 #  we have shown in class that, for all formula A, len(subformulas(A)) <= length(A).
 
 
 def atoms(formula):
+
     """Returns the set of all atoms occurring in a formula.
 
     For example, observe the piece of code below.
@@ -52,7 +52,13 @@ def atoms(formula):
     This piece of code above prints: p, s
     (Note that there is no repetition of p)
     """
-    pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
+    # pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
+    if isinstance(formula, Atom):
+        return {formula.__str__()}
+    if isinstance(formula, Not):
+        return atoms(formula.inner)
+    if isinstance(formula, Implies) or isinstance(formula, And) or isinstance(formula, Or):
+        return atoms(formula.left).union(atoms(formula.right))
 
 
 def number_of_atoms(formula):
@@ -63,16 +69,40 @@ def number_of_atoms(formula):
     must return 3 (Observe that this function counts the repetitions of atoms)
     """
     pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
+    if isinstance(formula, Atom):
+        return 1
+    if isinstance(formula, Not):
+        if isinstance(formula.inner, Atom):
+            return 1
+        else:
+            return number_of_atoms(formula.inner)
+    if isinstance(formula, Implies) or isinstance(formula, And) or isinstance(formula, Or):
+        return number_of_atoms(formula.left) + length(formula.right)
 
 
 def number_of_connectives(formula):
     """Returns the number of connectives occurring in a formula."""
-    pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
+    # pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
+    if isinstance(formula, Not):
+        return number_of_connectives(formula.inner) + 1
+    if isinstance(formula, Implies) or isinstance(formula, And) or isinstance(formula, Or):
+        return number_of_connectives(formula.left) + number_of_connectives(formula.right) + 1
+    if isinstance(formula, Atom):
+        return 0
 
 
 def is_literal(formula):
     """Returns True if formula is a literal. It returns False, otherwise"""
-    pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
+    # pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
+    if isinstance(formula, Atom):
+        return True
+    elif isinstance(formula, Not):
+        if isinstance(formula.inner, Atom):
+            return True
+        else:
+            return False
+    else:
+        return False
 
 
 def substitution(formula, old_subformula, new_subformula):
@@ -83,33 +113,77 @@ def substitution(formula, old_subformula, new_subformula):
 
 def is_clause(formula):
     """Returns True if formula is a clause. It returns False, otherwise"""
-    pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
+    #pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
+    if isinstance(formula, Or):
+        return is_clause(formula.left) and is_clause(formula.right)
+    if is_literal(formula):
+        return True
+    else:
+        return False
 
 
 def is_negation_normal_form(formula):
     """Returns True if formula is in negation normal form.
     Returns False, otherwise."""
-    pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
+    # pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
+    if isinstance(formula, Not):
+        if isinstance(formula.inner, And) or isinstance(formula.inner, Or) or isinstance(formula.inner, Implies):
+            return False
+        else:
+            return True
+    if isinstance(formula, And) or isinstance(formula, Or):
+        return is_negation_normal_form(formula.left) and is_negation_normal_form(formula.right)
+    if isinstance(formula, Atom):
+        return True
 
 
 def is_cnf(formula):
     """Returns True if formula is in conjunctive normal form.
     Returns False, otherwise."""
-    pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
+    #pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
+    if isinstance(formula, And):
+        return is_cnf(formula.left) and is_cnf(formula.right)
+    elif is_clause(formula):
+        return True
+    else:
+        return False
 
 
 def is_term(formula):
     """Returns True if formula is a term. It returns False, otherwise"""
-    pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
+    #pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
+    if isinstance(formula, And):
+        return is_term(formula.left) and is_term(formula.right)
+    elif is_literal(formula):
+        return True
+    else:
+        return False
 
 
 def is_dnf(formula):
     """Returns True if formula is in disjunctive normal form.
     Returns False, otherwise."""
-    pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
+    #pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
+    if isinstance(formula, Or):
+        return is_dnf(formula.left) and is_dnf(formula.right)
+    elif is_term(formula):
+        return True
+    else:
+        return False
 
 
 def is_decomposable_negation_normal_form(formula):
     """Returns True if formula is in decomposable negation normal form.
     Returns False, otherwise."""
-    pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
+    #pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
+    if is_negation_normal_form(formula):
+        if isinstance(formula, And):
+            if len(set(atoms(formula.left)).intersection(atoms(formula.right))) == 0:
+                return True
+            else:
+                return False
+        elif isinstance(formula, Or):
+            return is_decomposable_negation_normal_form(formula.left) and is_decomposable_negation_normal_form(formula.right)
+    else:
+        return False
+
